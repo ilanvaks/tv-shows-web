7471ed9dc34d7111388986582e8b8606a82d70c0
IlanVaks <ilanvaks@gmail.com>
2023-04-27 17:29:52 -0400

    Added Web-Setup

---
+++ src/scenes/SignUp.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react"
+
+export default function SignUp ( {setUser} ) {
+
+  const [email, setEmail]= useState("")
+  const [password, setPassword]= useState("")
+
+  const handleSignUp = (e) => {
+    e.preventDefault()
+    fetch("https://tv-shows-api-iv.web.app/signup", {
+      method: "POST", 
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify( {email, password} )
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      if(data.message) {
+        alert(data.message)
+        return
+      }
+        setUser(data)
+    })
+      .catch(alert)
+  }
+
+  return (
+    <>
+      <h2>SignUp</h2>
+        <form onSubmit={handleSignUp}>
+          <label htmlFor="email">Email 
+          <input 
+          type="text"
+          value={email}
+          onChange= { (e) => {setEmail(e.target.value)}} />
+          </label>
+
+          <br /> 
+
+          <label htmlFor="password">Password  
+            <input
+            type="password"
+            value={password}
+            onChange={ (e) => {setPassword(e.target.value)}} />
+            </label>
+
+            < br/>
+              <input type="submit" value="Sign Up" />
+        </form>
+    </>
+  )
+}
